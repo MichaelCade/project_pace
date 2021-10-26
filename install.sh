bd4d6fbb23a25577f8d0c7e3ec98585c8cbc6bd0
michaelcade <michael.cade@outlook.com>
2021-10-26 18:58:38 +0100

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
+echo "Create new cluster"
 minikube start --addons volumesnapshots,csi-hostpath-driver --apiserver-port=6443 --container-runtime=containerd -p mc-demo --kubernetes-version=1.21.2 
+
+echo "Deploy Kasten K10"
 
 helm repo add kasten https://charts.kasten.io/
 
 kubectl create namespace kasten-io
 helm install k10 kasten/k10 --namespace=kasten-io --set auth.tokenAuth.enabled=true --set injectKanisterSidecar.enabled=true --set-string injectKanisterSidecar.namespaceSelector.matchLabels.k10/injectKanisterSidecar=true
 
+echo "Annotate Volumesnapshotclass"
+
 kubectl annotate volumesnapshotclass csi-hostpath-snapclass \
     k10.kasten.io/is-snapshot-class=true
+
+echo "Change default storageclass"
 
 kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+echo "Deploy MySQL"
+
+APP_NAME=my-production-app
+kubectl create ns ${APP_NAME}
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install mysql-store bitnami/mysql --set primary.persistence.size=1Gi,volumePermissions.enabled=true --namespace=${APP_NAME}
+kubectl get pods -n ${APP_NAME}
+
+echo "Waiting 5 mins for pod to come up"
+sleep 5m
+
+kubectl get pods -n ${APP_NAME}
+
+echo "MySQL root password"
+
+MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace ${APP_NAME} mysql-store -o jsonpath="{.data.mysql-root-password}" | base64 --decode)
+MYSQL_HOST=mysql-store.${APP_NAME}.svc.cluster.local
+MYSQL_EXEC="mysql -h ${MYSQL_HOST} -u root --password=${MYSQL_ROOT_PASSWORD} -DmyImportantData -t"
+echo MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+
+echo "Environment Complete"
