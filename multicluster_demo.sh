f9e518e79f277ca58ee912e811f009cd2049c5b3
michaelcade <michael.cade@outlook.com>
2021-10-29 16:37:10 +0100

    add multi-cluster bash script

--- multicluster_demo.sh
+++ multicluster_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Create new cluster"
+echo "Create cluster 1 "
 minikube start --addons volumesnapshots,csi-hostpath-driver --apiserver-port=6443 --container-runtime=containerd -p mc-demo --kubernetes-version=1.21.2 
 
 echo "Deploy Kasten K10"
@@ -39,5 +39,29 @@ MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace ${APP_NAME} mysql-store -o 
 MYSQL_HOST=mysql-store.${APP_NAME}.svc.cluster.local
 MYSQL_EXEC="mysql -h ${MYSQL_HOST} -u root --password=${MYSQL_ROOT_PASSWORD} -DmyImportantData -t"
 echo MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+
+echo "Create cluster 2"
+minikube start --addons volumesnapshots,csi-hostpath-driver --apiserver-port=6443 --container-runtime=containerd -p mc-demo2 --kubernetes-version=1.21.2 
+
+echo "Deploy Kasten K10"
+
+helm repo add kasten https://charts.kasten.io/
+
+kubectl create namespace kasten-io
+helm install k10 kasten/k10 --namespace=kasten-io --set auth.tokenAuth.enabled=true --set injectKanisterSidecar.enabled=true --set-string injectKanisterSidecar.namespaceSelector.matchLabels.k10/injectKanisterSidecar=true
+
+echo "Annotate Volumesnapshotclass"
+
+kubectl annotate volumesnapshotclass csi-hostpath-snapclass \
+    k10.kasten.io/is-snapshot-class=true
+
+echo "Change default storageclass"
+
+kubectl patch storageclass csi-hostpath-sc -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
+
+
 
 echo "Environment Complete"
