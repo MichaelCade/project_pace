b18aefa81cea02d4aef8231dd13b409d250ff110
Michael Cade <Michael.Cade@outlook.com>
2021-11-18 16:36:03 +0000

    add echo for exec commands

--- AddDatatoMongo.sh
+++ AddDatatoMongo.sh
@@ -1,5 +1,9 @@
 echo "Add Data to MongoDB"
+
+echo "Add the following lines into the context of the container to add data" 
+echo "mongo admin --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD --quiet --eval "db.restaurants.insert({'name' : 'Roys', 'cuisine' : 'Hawaiian', 'id' : '8675309'})""
+echo "mongo admin --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD --quiet --eval "db.restaurants.find()""
+
 kubectl exec -ti my-release-mongodb-0 -n mongo-test -- bash
-mongo admin --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD --quiet --eval "db.restaurants.insert({'name' : 'Roys', 'cuisine' : 'Hawaiian', 'id' : '8675309'})"
-mongo admin --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD --quiet --eval "db.restaurants.find()"
+
 exit 
